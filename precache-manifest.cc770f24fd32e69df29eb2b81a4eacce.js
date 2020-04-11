360cf7f223aee5b307bafffab318decd935cc4eb
Muhammad Fakhruz Razi <fakhruz@wenconsulting.com>
2020-04-12 00:52:04 +0800

    Updates

---
+++ precache-manifest.cc770f24fd32e69df29eb2b81a4eacce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4058b083753309d1b56fe1567e593ad",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e498966a44550344279a",
+    "url": "/portfolio/static/css/main.fed92599.chunk.css"
+  },
+  {
+    "revision": "9ac667ceb082c167dab2",
+    "url": "/portfolio/static/js/2.78e1a0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.78e1a0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e498966a44550344279a",
+    "url": "/portfolio/static/js/main.2b10d994.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  }
+]);
